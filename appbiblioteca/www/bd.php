beb67f300f1d26f71dfbb6e797da09633f447258
nandabalassoni <mariafernandabalassoni@hotmail.com>
2020-07-08 01:56:01 -0300

    arquivo com conexao com o banco de dados

--- appbiblioteca/www/bd.php
+++ appbiblioteca/www/bd.php
@@ -2,7 +2,7 @@
 
 	$con = mysqli_connect("localhost", "root", "", "projetobiblioteca") or die ("Não foi possível conectar".mysqli_connect_error());
 
-    // mysqli_close($con);
+     //mysqli_close($con);
 
    
 ?>
