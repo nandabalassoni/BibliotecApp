3c6198a2db292fa428578efd701968375e3a5fcd
nandabalassoni <mariafernandabalassoni@hotmail.com>
2020-07-08 02:00:49 -0300

    arquivo para excluir dados do banco

---
+++ appbiblioteca/www/delete.php
@@ -0,0 +1,17 @@
+<?php
+
+	include 'bd.php';
+
+	$idaluno = $_GET['idaluno'];
+
+	$delete_query="DELETE FROM aluno WHERE idaluno ='$idaluno'";
+
+	$result_delete=(mysqli_query($con, $delete_query));
+
+	if($result_delete)
+		echo "<script> location.href='aluno.php?p=inicial'</script>";
+	else
+		echo "<script> alert('Não foi possível deletar usuário.');location.href='exibe.php?p=inicial';
+			</script>";
+
+?>
