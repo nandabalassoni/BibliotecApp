070ecd42dcd0a5992c795674a9f89a5f9671aa8f
nandabalassoni <mariafernandabalassoni@hotmail.com>
2020-07-08 01:54:40 -0300

    atualizando o modal para cadastro - incluindo a conexao com o banco

--- appbiblioteca/www/aluno.php
+++ appbiblioteca/www/aluno.php
@@ -77,6 +77,8 @@
                                         <td><?php echo($aluno['nome']); ?></td>
                                         <td><?php echo($aluno['ra']); ?></td>				
                                         <td><?php echo($aluno['cpf']); ?></td>
+                                        <td><a href="delete.php?idaluno=<?php echo $aluno['idaluno']; ?>">Excluir</a>
+										<a href="edit.php?idcontato=<?php echo $contato['idcontato']; ?>">Editar</a></td>
                                     </tr>	
                                     <?php endforeach; ?>				
                             <?php } ?>
@@ -94,43 +96,53 @@
 
             <!-- Cadastro Modal -->
 
-            <div class="modal fade" id="cadastroAlunoModal" tabindex="-1" role="dialog" aria-labelledby="cadastroAlunoModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="cadastroAlunoModalLabel">Cadastrar novo aluno</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            <form action="bd.php" method="post" id="cadastro">
-                                <div class="form-group">
-                                    <label for="recipient-name" class="col-form-label">Nome</label>
-                                    <input type="text" class="form-control" placeholder="Nome do aluno" name="nome" required />
-                                </div>
-                                <div class="form-group">
-                                    <label for="recipient-ra" class="col-form-label">RA</label>
-                                    <input type="text" class="form-control"  placeholder="RA do aluno" name="ra" required />
-                                </div>
-                                <div class="form-group">
-                                    <label for="recipient-cpf" class="col-form-label">CPF</label>
-                                    <input type="text" class="form-control"  placeholder="CPF do aluno" name="cpf" required />
-                                </div>
-                            </form>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-                            <!--button type="button" class="btn btn-primary" data-dismiss="modal">Enviar</button-->
-                            <input type="submit" value ="Enviar" class="btn btn-primary" data-dismiss="modal"/>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <div id="cadastroAlunoModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title" id="cadastroAlunoModalLabel">Cadastrar Aluno</h5>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						  <span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+					<div class="modal-body">
+						<form method="post" id="insert_form" action="cadastrarAluno.php">
+							<div class="form-group row">
+								<label class="col-sm-2 col-form-label">Nome</label>
+								<div class="col-sm-10">
+									<input name="nome" type="text" class="form-control" id="nome" placeholder="Nome completo">
+								</div>
+							</div>
+							
+							<div class="form-group row">
+								<label class="col-sm-2 col-form-label">RA</label>
+								<div class="col-sm-10">
+									<input name="ra" type="text" class="form-control" id="ra" placeholder="RA do aluno">
+								</div>
+                            </div>
 
+                            <div class="form-group row">
+								<label class="col-sm-2 col-form-label">CPF</label>
+								<div class="col-sm-10">
+									<input name="cpf" type="text" class="form-control" id="cpf" placeholder="CPF do aluno">
+								</div>
+                            </div>
+                            
+							<div class="form-group row">
+								<div class="col-sm-10">
+									<input type="submit" name="CadUser" id="CadUser" value="Cadastrar" class="btn btn-outline-success">
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
         </div>
+
+    </div>
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+            
     </body>
 </html>
